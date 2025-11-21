2bf6992d90f6919f7f436a1cbf464988d5901ee5
mlakusss <mironov.aptem@yandex.ru>
2025-11-22 02:49:19 +0300

    Обновил тесты под актуальную версию проекта

--- SmartGreenhouse/SmartGreenhouse.Tests/Core/BooleanLogicTests.cs
+++ SmartGreenhouse/SmartGreenhouse.Tests/Core/BooleanLogicTests.cs
@@ -8,20 +8,18 @@ namespace SmartGreenhouse.Tests.Core
     public class BooleanLogicTests
     {
         [Theory]
-        [InlineData(10.0, false, 1800, true)]   // Низкая температура + ночь + низкий CO2
-        [InlineData(20.0, false, 1800, false)]  // Нормальная температура
-        [InlineData(10.0, true, 1800, false)]   // Низкая температура + день
-        [InlineData(10.0, false, 2200, false)]  // Низкая температура + ночь + высокий CO2
-        public void ShouldTurnOnHeating_CorrectConditions(double temp, bool isDayTime, double co2, bool expected)
+        [InlineData(10.0, true)]   // Низкая температура (< 15°C) - нужен обогрев
+        [InlineData(5.0, true)]     // Очень низкая температура (< 15°C) - нужен обогрев
+        [InlineData(14.9, true)]     // Температура чуть ниже порога (< 15°C) - нужен обогрев
+        [InlineData(15.0, false)]    // Температура на пороге (>= 15°C) - обогрев не нужен
+        [InlineData(20.0, false)]    // Нормальная температура (>= 15°C) - обогрев не нужен
+        [InlineData(25.0, false)]   // Высокая температура (>= 15°C) - обогрев не нужен
+        public void ShouldTurnOnHeating_CorrectConditions(double temp, bool expected)
         {
             // Arrange
             var data = new SensorData
             {
-                Temperature = temp,
-                CO2Level = co2,
-                Timestamp = isDayTime ?
-                    DateTime.Parse("2024-01-01 12:00") :
-                    DateTime.Parse("2024-01-01 20:00")
+                Temperature = temp
             };
 
             // Act
@@ -32,9 +30,11 @@ namespace SmartGreenhouse.Tests.Core
         }
 
         [Theory]
-        [InlineData(25.0, true)]   // Низкая влажность почвы
-        [InlineData(40.0, false)]  // Средняя влажность
-        [InlineData(60.0, false)]  // Высокая влажность
+        [InlineData(25.0, true)]   // Низкая влажность почвы (< 60%)
+        [InlineData(40.0, true)]   // Средняя влажность (< 60%)
+        [InlineData(55.0, true)]   // Влажность чуть ниже порога (< 60%)
+        [InlineData(60.0, false)]  // Влажность на пороге (>= 60%)
+        [InlineData(70.0, false)]  // Высокая влажность (>= 60%)
         public void ShouldTurnOnWater_CorrectConditions(double soilMoisture, bool expected)
         {
             // Arrange
--- SmartGreenhouse/SmartGreenhouse.Tests/obj/Debug/net8.0/SmartGreenhouse.Tests.AssemblyInfo.cs
+++ SmartGreenhouse/SmartGreenhouse.Tests/obj/Debug/net8.0/SmartGreenhouse.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4fbbd2258e33cbe825757ba1b7db505fa892df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02830cc64f08105d82a90b74d9a9b28172adc95a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
