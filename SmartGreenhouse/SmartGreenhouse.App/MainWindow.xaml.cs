b2a6acf3082731a71a54733fb035b5fab3f16ad7
Ereb <erebfr@gmail.com>
2025-11-21 21:28:17 +0300

    Выполнен Frontend

--- SmartGreenhouse/SmartGreenhouse.App/MainWindow.xaml.cs
+++ SmartGreenhouse/SmartGreenhouse.App/MainWindow.xaml.cs
@@ -13,16 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace SmartGreenhouse.App
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
--- SmartGreenhouse/SmartGreenhouse.Tests/obj/Debug/net8.0/SmartGreenhouse.Tests.AssemblyInfo.cs
+++ SmartGreenhouse/SmartGreenhouse.Tests/obj/Debug/net8.0/SmartGreenhouse.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bb804e2fe798319bc5752e0300bd5fd670bd9d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af6ca4644d4ce205ba5db4209477623b3646e96")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
