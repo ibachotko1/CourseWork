e82feae82ece30bbcf5e64e129b6bae36398db1f
KockAnigilator <jonikm892@gmail.com>
2025-11-21 14:03:21 +0300

    Инициализация

---
+++ SmartGreenhouse/SmartGreenhouse.Services/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGreenhouse.Services
+{
+    public class Class1
+    {
+    }
+}
