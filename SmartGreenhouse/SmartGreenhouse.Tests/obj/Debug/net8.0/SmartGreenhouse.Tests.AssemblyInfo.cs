3715f3284921c5e45c6241baad0595fd46ac3831
Ereb <erebfr@gmail.com>
2025-11-22 11:00:04 +0300

    Пробная версия

--- SmartGreenhouse/SmartGreenhouse.Tests/obj/Debug/net8.0/SmartGreenhouse.Tests.AssemblyInfo.cs
+++ SmartGreenhouse/SmartGreenhouse.Tests/obj/Debug/net8.0/SmartGreenhouse.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02830cc64f08105d82a90b74d9a9b28172adc95a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf6992d90f6919f7f436a1cbf464988d5901ee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
